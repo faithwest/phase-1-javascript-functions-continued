aea957a09ac253372c6c7f063ab66c4007f7cde1
faithwest <faithchepkoech425@gmail.com>
2023-09-20 15:07:15 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+
+  function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  function wrapAdjective(wrapper = "*") {
+    return function (adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
 // code your solution here
